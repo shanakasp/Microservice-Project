96c46de0d34275973c5f033427cec552acec7eaf
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-15 10:31:17 +0530

    modified App.js and index.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Post from "./pages/post/index.jsx";
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1>Create a Post</h1>
       <Post></Post>
     </div>
--- client/src/pages/post/index.jsx
+++ client/src/pages/post/index.jsx
@@ -1,12 +1,24 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useState } from "react";
 const Index = () => {
+  const [title, settitle] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(title);
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    settitle("");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label>Title</label>
           <input
+            value={title}
+            onChange={(e) => settitle(e.target.value)}
             className="form-control"
             type="text"
             placeholder="Title"
